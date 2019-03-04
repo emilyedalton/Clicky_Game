76bbeb471447f1252468e4549d4a4dc23ba1275e
Emily Dalton <emilydalton@dhcp-10-105-61-102.wireless.northwestern.private>
2019-03-04 14:15:03 -0600

    Updates

---
+++ precache-manifest.50f8184e264c2e2a85f36d3338d816ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky_game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f8cf428aeba88790c600",
+    "url": "/clicky_game/static/js/runtime~main.f8cf428a.js"
+  },
+  {
+    "revision": "0c72ab9eccd84089a57a",
+    "url": "/clicky_game/static/js/main.0c72ab9e.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/clicky_game/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "0c72ab9eccd84089a57a",
+    "url": "/clicky_game/static/css/main.497ed3e9.chunk.css"
+  },
+  {
+    "revision": "87b49e025855c22eec1e177228900858",
+    "url": "/clicky_game/index.html"
+  }
+];
