c87388230493457c112af05be0f91092aea48c7f
Emily Dalton <emilydalton@dhcp-10-105-61-102.wireless.northwestern.private>
2019-03-06 08:44:04 -0600

    adding begining of function that will control score

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Scorecount from "./components/Scorecount";
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import friends from "./friends.json";
 
 class App extends Component {
+  // Setting this.state.friends to the friends json array
+  //score = 0
+  //topscore = 0
+  // on click 
+  // .sort(()=>Math.random()-0.5));
+  state = {
+    friends,
+     score: 0,
+  topscore: 0
+  };
+  handleIncrement = () => {
+   
+    // We always use the setState method to update a component's state
+    this.setState({ score: this.state.friends.score + 1 });
+    // this.setState({ friends });
+
+  };
+  
+  removeFriend = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    // const friends = this.state.friends.filter(friend => friend.id !== id);
+    // Set this.state.friends equal to the new friends array
+shuffle(friends);  
+this.setState({ friends , score: this.state.score + 1});
+  };
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Wrapper>
+  {/* each piece of data needs to be specified in the return 
+  inside of the component listed as a property. Then called as {props.whatever}in
+  the component */}
+              <Scorecount 
+              score={this.state.score}
+              link ={this.state.link}
+              />
+
+        <Title>Friends List score: {this.state.score}</Title>
+        {this.state.friends.map(friend => (
+          <FriendCard
+            removeFriend={this.removeFriend}
+            id={friend.id}
+            key={friend.id}
+            name={friend.name}
+            image={friend.image}
+            occupation={friend.occupation}
+            location={friend.location}
+            score={friend.score}
+          />
+        ))}
+      </Wrapper>
     );
   }
 }
