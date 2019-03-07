1d3316e38f2a25e09d8ec49e844200d326df7450
Emily Dalton <emilydalton@dhcp-10-105-61-102.wireless.northwestern.private>
2019-03-07 12:17:04 -0600

    adding finished navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,19 +1,19 @@
 import React from 'react';
-
+import "./style.css"
 
 function Navbar(props){
     return(
-    <div>
-        <nav class = "navbar navbar-expand-lg navbar-light bg-light">
+    <div class = "jumbotron">
+        {/* <nav class = "navbar navbar-expand-lg navbar-light bg-light"> */}
     <header> 
     
-        <h2>Memory Game</h2>
+        <h2>МЕМОРЫ  ГАМЕ!</h2>
         {props.children}
            {/* <button onClick ={props.NewGame}>New Game</button> */}
         
     </header>
-    </nav>
-    </div>);
+    </div>
+    );
     
     }
 
